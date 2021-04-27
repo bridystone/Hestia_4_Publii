acfd9e24cb7ae0fa9f8f7eca758a041423b99894
Jens Breidenstein <github@breidentstein.info>
2021-04-27 18:19:05 +0200

    first ideas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,18 @@
-// This is your main script file
+/* 
+fixes missing wordpress response in script.min.js
+script.min.js disables autoslide, when in customizer mode
+the response of ajax.php is requested for this
+*/
+let requestpost = {
+    disable_autoslide : 1
+};
+
+/* set the interval of sliders */
+
+var settings = {
+    interval: 3000
+};
+
+$( '.carousel' ).carousel(
+    settings
+);
